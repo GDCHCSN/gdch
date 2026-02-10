75ffdb297ed887e4215126be21027167a5702f16
Suyash Singh <suyashsingh.mit@gmail.com>
2026-02-10 18:11:41 +0530

    added new documents

--- procurement-cell/index.php
+++ procurement-cell/index.php
@@ -60,6 +60,7 @@ $CURRENTDIRURL = $ROOTURL . "procurement-cell/";
                     <li><a href="<?php echo $CURRENTDIRURL ?>assets/2025-26/संगणक, प्रिंटर, सीसीटीव्ही वार्षिक सेवा करारासाठी दरपत्रक मोहोदबंद लिफाफे सादर करणेबाबत.pdf" target="_blank">संगणक, प्रिंटर, सीसीटीव्ही वार्षिक सेवा करारासाठी दरपत्रक मोहोदबंद लिफाफे सादर करणे</a></li>
                     <li><a href="<?php echo $CURRENTDIRURL ?>assets/2025-26/Books Quotation 2026.pdf" target="_blank">Books Quotation 2026</a></li>
                     <li><a href="<?php echo $CURRENTDIRURL ?>assets/2025-26/Journal Quotation 2026.pdf" target="_blank">Journal Quotation 2026</a></li>
+                    <li><a href="<?php echo $CURRENTDIRURL ?>assets/2025-26/उपकरणे_संसाधने यांचे खरेदीच्या अनुषंगाने दरपत्रक मोहोरबंद लिफाफे सादर करणेबाबत.pdf" target="_blank">उपकरणे संसाधने यांचे खरेदीच्या अनुषंगाने दरपत्रक मोहोरबंद लिफाफे सादर करणेबाबत</a></li>
                 </ul>
                 </ul>
                 </ul>
