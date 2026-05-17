1276569164ccdefe40b1d57cbf70c98b35870e3a
Suyash Singh <suyashsingh.mit@gmail.com>
2026-05-17 18:25:25 +0530

    added new tab name collage opd

--- about-us/about-college/index.php
+++ about-us/about-college/index.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-$ROOT = "../../"; 
+$ROOT = "../../";
 include($ROOT . "includes/_init.php");
 $CURRENTDIRURL = $ROOTURL . "about-us/about-college/";
 
@@ -8,6 +8,7 @@ $CURRENTDIRURL = $ROOTURL . "about-us/about-college/";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,34 +17,41 @@ $CURRENTDIRURL = $ROOTURL . "about-us/about-college/";
 
     <script src="<?php echo $ROOTURL ?>public/js/_navbar.js" defer></script>
     <script src="<?php echo $ROOTURL ?>public/js/departments/departmentMain.js" defer></script>
-    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/global.css"></link>
-    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_navbar.css"></link>
-    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_footer.css"></link>
-    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/about-us/about-us.css"></link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/global.css">
+    </link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_navbar.css">
+    </link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_footer.css">
+    </link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/about-us/about-us.css">
+    </link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/departments/departmentStyles.css">
+    </link>
 </head>
+
 <body>
 
     <?php include($ROOT . "includes/_navbar.php"); ?>
 
     <div class="pageBanner">
-        <img src="<?php echo $ROOTURL ?>public/assets/pageBanner.jpg" loading="lazy"/>
+        <img src="<?php echo $ROOTURL ?>public/assets/pageBanner.jpg" loading="lazy" />
         <h1>
             ABOUT COLLEGE
         </h1>
-        
+
     </div>
     <div>
         <div class="main">
             <div class="about-us-container">
                 <h3 class="college-title">Government Dental College & Hospital Chhatrapati Sambhajinagar</h3>
-                <img class="about-img" src="<?php echo $ROOTURL ?>assets/Carousel8.jpg" alt="" style="border-radius:10px; width:95%;"  height="500" loading="lazy"></img>
+                <img class="about-img" src="<?php echo $ROOTURL ?>assets/Carousel8.jpg" alt="" style="border-radius:10px; width:95%;" height="500" loading="lazy"></img>
                 <p class="college-desc">
                     Govt Dental College and Hospital, Chhatrapati Sambhajinagar is one of the reputed Govt. Dental Colleges in Maharashtra. The college is popularly known as GDCH Chhatrapati Sambhajinagar and was founded in the year 1982-83. The college was established to fortify the weakest section of the Nation, i.e. Rural population. It was started to serve students with the best dental care and Oral and Maxillofacial Surgery. The college is affiliated with the Maharashtra University of Health Sciences, Nashik, and is approved by the Dental Council of India, Govt. of India.
                 </p>
-                
-                
+
+
                 <h3 class="dept-title">Departments</h3>
-                
+
                 <div class="departmentsList">
                     <ul>
                         <li>Oral Medicine and Radiology</li>
@@ -51,25 +59,53 @@ $CURRENTDIRURL = $ROOTURL . "about-us/about-college/";
                         <li>Oral and Maxillofacial Pathology & Oral Microbiology</li>
                         <li>Periodontology</li>
                         <li>Community Dentistry </li>
-                        
-                        
+
+
                     </ul>
                     <ul>
                         <li>Conservative Dentistry and Endodontics</li>
                         <li>Pediatric and Preventive Dentistry</li>
                         <li>Orthodontics and Dentofacial Orthopedics</li>
                         <li>Prosthodontics</li>
-                       
+
                     </ul>
                 </div>
-                
-                
             </div>
-            
+        </div>
     </div>
-    
+
+    <div class="opdMaindiv">
+        <div class="facultyContainer marginTop">
+            <div class="facultyToggleBtn">
+                <img class="plusIcon" src="<?php echo $ROOTURL ?>public/assets/plus-icon.svg" loading="lazy" />
+                <img class="minusIcon" src="<?php echo $ROOTURL ?>public/assets/minus-icon.svg" loading="lazy" />
+                <h2>College OPD</h2>
+            </div>
+
+            <div class="accordionContent">
+                <div class="opdTableBox">
+                    <table>
+                        <tr>
+                            <th>Month</th>
+                            <th>College OPD</th>
+                        </tr>
+                        <tr>
+                            <td>April</td>
+                            <td>6756</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 
     <?php include($ROOT . "includes/_footer.php"); ?>
 
 </body>
+
 </html>
